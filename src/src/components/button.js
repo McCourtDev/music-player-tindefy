fe1c2cf9c9d46bf6ba7749029f09e537c4e520c0
Bradley McCourt <45145756+McCourtDev@users.noreply.github.com>
2023-04-09 13:51:13 +0100

    Delete src/src directory

--- src/src/components/button.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { BsFillHandThumbsDownFill } from "react-icons/bs";
-
-<button>
-  <BsFillHandThumbsDownFill className=" fill-primary hover:fill-blue-600" />
-</button>;
